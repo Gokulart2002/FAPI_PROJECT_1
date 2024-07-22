d595992f90cca47d58a5579341358ea0e8f5adfa
gokulart2002 <gokulart2002@gmail.com>
2024-07-22 16:33:59 +0530

    i create demofastapi

--- demofastapi/Scripts/activate.bat
+++ demofastapi/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=A:\gokul\Mindfulai work\project\Fastapi\project_1\demofastapi
+set VIRTUAL_ENV=A:\gokul\Mindfulai work\project\Fastapi\Fapi_project_1\demofastapi
 
 if not defined PROMPT set PROMPT=$P$G
 
